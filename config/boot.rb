7c63d1a0df310eb0005246d1d54cf55a544032a0
Cesar Figueroa <cesar.figueroa@me.com>
2013-07-20 07:09:47 -0700

    Autoload conditions

--- config/boot.rb
+++ config/boot.rb
@@ -9,5 +9,6 @@ Dir.glob('app/helpers/*.rb').each { |h| require h }
 require 'config/middleware'
 require 'config/application'
 
+Dir.glob('app/helpers/conditions/*.rb').each { |c| require c }
 Dir.glob('app/models/*.rb').each { |m| require m }
 Dir.glob('app/routes/*.rb').each { |r| require r }
