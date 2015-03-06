f85183bf5cdfeb62ede008a2e5cfae3a157a4dac
Cesar Figueroa <cesar.figueroa@me.com>
2015-03-06 10:00:44 -0600

    Remove dependency on ActiveSupport's #demodulize

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,7 +1,7 @@
 module Acme
   module Helpers
     def template(name, *args)
-      directory = self.class.name.demodulize.downcase
+      directory = self.class.name.split('::').last.downcase
       erb File.join(directory, name.to_s).to_sym, *args
     end
   end
