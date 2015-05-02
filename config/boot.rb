786a29510d5b165f783ac8e852c747cfd34ed40c
Cesar Figueroa <cesar.figueroa@me.com>
2015-05-02 13:00:58 -0500

    Only autoload initializers
    This prevents initializers like Puma's config file from being loaded by the
    application.

--- config/boot.rb
+++ config/boot.rb
@@ -2,7 +2,7 @@ require 'bundler'
 
 Bundler.require(:default, ENV['RACK_ENV'] || 'development')
 
-Dir.glob('config/**/*.rb').each { |file| require file }
+Dir.glob('config/initializers/*.rb').each { |file| require file }
 
 Dir.glob('app/**/*.rb').each do |file|
   unless Dir.entries('app/routes').include?(File.basename(file))
@@ -12,5 +12,8 @@ end
 
 require 'app/routes/base'
 Dir.glob('app/routes/*.rb').each { |file| require file }
+
+require 'config/middleware'
+require 'config/routes'
 
 require 'application'
