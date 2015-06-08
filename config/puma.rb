adf57c3b5f922c57d6f5e90dec398b3c2b4efd9e
Cesar Figueroa <cesar.figueroa@me.com>
2015-06-08 07:20:01 -0500

    Add default values to Puma configuration

--- config/puma.rb
+++ config/puma.rb
@@ -1,12 +1,12 @@
-workers ENV['SERVER_WORKER_COUNT']
+workers ENV['SERVER_WORKER_COUNT'] || 2
 
-threads_count = ENV['SERVER_MAX_THREADS_COUNT']
+threads_count = ENV['SERVER_MAX_THREADS_COUNT'] || 5
 threads threads_count, threads_count
 
 preload_app!
 
-port ENV['SERVER_PORT']
-environment ENV['RACK_ENV']
+port ENV['SERVER_PORT'] || 3000
+environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
   Sequel.connect(ENV['DATABASE_URL'])
